082e9e4be4ac9f27484d78bf33cbb6f02f2a059f
Andy <voandy1998@gmail.com>
2018-04-24 01:11:38 +0000

    implement turns

--- player.rb
+++ player.rb
@@ -1,10 +1,15 @@
 class Player
-  attr_reader :lives
-  def initialize
+  attr_reader :lives, :turn
+  def initialize(t)
     @lives = 3
+    @player_number = t
   end
 
   def lose_hp
-    self.lives -= 1
+    @lives -= 1
+  end
+
+  def display_hp
+    puts "P#{@player_number} #{@lives}/3"
   end
 end
--- question.rb
+++ question.rb
@@ -3,17 +3,14 @@ class Question
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
-    puts "What is #{@num1} + #{@num2}?"
+    # puts "What is #{@num1} + #{@num2}?"
   end
 
   def verify(guess)
     if(@num1 + @num2 == guess)
-      puts true
+      true
     else
-      puts false
+      false
     end
   end
 end
-
-q1 = Question.new
-q1.verify(gets.chomp.to_i)
